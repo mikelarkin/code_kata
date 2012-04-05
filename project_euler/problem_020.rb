7b180839b4e457d00d27528baff75f49c4afefc1
Michael Larkin <mjlarkin@gmail.com>
2012-04-05 11:11:12 -0400

    Problem stubs

--- project_euler/problem_020.rb
+++ project_euler/problem_020.rb
@@ -3,7 +3,11 @@
 # For example, 10! = 10 x 9 x ... x 3 x 2 x 1 = 3628800,
 # and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
 #
-# Find the sum of the digits in the number 100!
+# Find the sum of the digits in the supplied number
+#
+# Written by http://github.com/mikelarkin
+
+
 limit = (ARGV.empty? ? 100 : ARGV[0].to_i)
 
 product = 1
--- project_euler/problem_022.rb
+++ project_euler/problem_022.rb
@@ -6,3 +6,5 @@
 # list. So, COLIN would obtain a score of 938 x 53 = 49714.
 #
 # What is the total of all the name scores in the file?
+#
+# Written by http://github.com/mikelarkin
