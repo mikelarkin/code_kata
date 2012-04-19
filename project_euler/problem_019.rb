40cc34377cf39bef800d295e3ae79103e1d913e6
Michael Larkin <mjlarkin@gmail.com>
2012-04-19 11:12:10 -0400

    Problem 19

--- project_euler/problem_019.rb
+++ project_euler/problem_019.rb
@@ -12,3 +12,14 @@
 # How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
 #
 # Written by http://github.com/mikelarkin
+
+# Cheat using Ruby Date class
+require 'date'
+count = 0
+1901.upto(2000).each do |year|
+  1.upto(12).each do |month|
+    count += 1 if Date.new(year, month, 1).sunday?
+  end
+end
+
+puts "Number of Sundays in the 20th century: #{count}"
