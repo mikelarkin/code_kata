30f5a6c2c5854592d4d78e297919eb3dd7e6564e
Michael Larkin <mjlarkin@gmail.com>
2015-08-06 11:52:08 -0400

    better commenting

--- project_euler/problem_007.rb
+++ project_euler/problem_007.rb
@@ -10,10 +10,17 @@ limit = nth_prime * 100
 
 # Initialize a large enough array
 sieve = Array.new(limit) { |i| i }
+
+# Go ahead and knock out 0 and 1
 sieve[0] = sieve[1] = nil
 
+# Iterate up to the sqrt of the array size (since anything beyond this will be a multiple of a previous number)
 2.upto(limit ** 0.5) do |i|
+
+  # Already marked off as not prime?
   next unless sieve[i]
+
+  # Mark off multiples of the current iterator since they are not prime
   (i*i).step(limit, i) do |j|
     sieve[j] = nil
   end
@@ -21,6 +28,6 @@ sieve[0] = sieve[1] = nil
 end
 
 
-# Remove nil (non-prime) numbers
+# Remove nil (non-prime) numbers by trimming the nils
 sieve.compact!
 puts "Prime ##{nth_prime} is #{sieve[nth_prime - 1]}"
